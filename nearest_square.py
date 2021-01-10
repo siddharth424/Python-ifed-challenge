7b9177ccf586c498838d36f322b1cca92abf1c47
siddharth424 <siddharthmaurya887@gmail.com>
2021-01-10 20:41:44 +0530

    Update nearest_square.py

--- nearest_square.py
+++ nearest_square.py
@@ -1,8 +1,14 @@
 import math
 def nearest_square(n):
-
+    
+    #If n is positive the square of integral part of square root will give the answer
     if n>0:
         i=int(math.sqrt(n))
         return (i*i)
+
+    # If n negative then the closest solution will be zero
     else:
         return 0
+
+nearest_square.__doc__ = """
+    This function finds the nearest perfect square number which is less than or equal to the number. """
